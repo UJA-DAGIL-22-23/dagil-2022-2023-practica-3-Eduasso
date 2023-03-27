7584dfdca6cfab6d1cfcc97f8713de2e9faad49a
Eduardo Lomas Recena <elr00030@red.ujaen.es>
2023-03-27 19:39:34 +0200

    HU-04 terminada

--- front-end/static-files/js/ms-plantilla.js
+++ front-end/static-files/js/ms-plantilla.js
@@ -18,6 +18,45 @@ Plantilla.datosDescargadosNulos = {
     fecha: ""
 }
 
+// Tags que voy a usar para sustituir los campos
+Plantilla.plantillaTags = {
+    "NOMBRE": "### NOMBRE ###",
+    "FECHA": {"DIA": "### DIA ###", "MES": "### MES ###", "ANIO": "### ANIO ###"},
+    "PAIS": "### PAIS ###",
+    "CUMBRES": "### CUMBRES ###",
+    "PICOS 8KM": "### PICOS_8KM ###"
+}
+
+/// Plantilla para poner los datos de varias personas dentro de una tabla
+Plantilla.plantillaTablaPersonas = {}
+
+// Cabecera de la tabla
+Plantilla.plantillaTablaPersonas.cabecera = `<table width="100%" class="listado-personas">
+                    <thead>
+                        <th width="10%">Nombre</th>
+                        <th width="20%">Fecha de nacimiento</th>
+                        <th width="20%">Pais de nacimiento</th>
+                        <th width="30%">Grandes Cumbres Alcanzadas</th>
+                        <th width="10%">Picos 8km</th>
+                    </thead>
+                    <tbody>
+    `;
+
+// Elemento TR que muestra los datos de una persona
+Plantilla.plantillaTablaPersonas.cuerpo = `
+    <tr title="${Plantilla.plantillaTags.NOMBRE}">
+        <td>${Plantilla.plantillaTags.NOMBRE}</td>
+        <td>${Plantilla.plantillaTags.FECHA.DIA}/${Plantilla.plantillaTags.FECHA.MES}/${Plantilla.plantillaTags.FECHA.ANIO}</td>
+        <td>${Plantilla.plantillaTags.PAIS}</td>
+        <td>${Plantilla.plantillaTags.CUMBRES}</td>
+        <td>${Plantilla.plantillaTags["PICOS 8KM"]}</td>
+    </tr>
+    `;
+
+// Pie de la tabla
+Plantilla.plantillaTablaPersonas.pie = `        </tbody>
+             </table>
+             `;
 
 /**
  * Función que descarga la info MS Plantilla al llamar a una de sus rutas
@@ -92,6 +131,74 @@ Plantilla.mostrarAcercaDe = function (datosDescargados) {
     Frontend.Article.actualizar("Plantilla Acerca de", mensajeAMostrar)
 }
 
+/**
+ * Actualiza el cuerpo de la plantilla deseada con los datos de la persona que se le pasa
+ * @param {String} Plantilla Cadena conteniendo HTML en la que se desea cambiar lso campos de la plantilla por datos
+ * @param {Persona} Persona Objeto con los datos de la persona que queremos escribir en el TR
+ * @returns La plantilla del cuerpo de la tabla con los datos actualizados 
+ */           
+Plantilla.sustituyeTags = function (plantilla, persona) {
+    return plantilla
+        .replace(new RegExp(Plantilla.plantillaTags.NOMBRE, 'g'), persona.data.Nombre)
+        .replace(new RegExp(Plantilla.plantillaTags.FECHA.DIA, 'g'), persona.data.Fecha_nacimiento.Dia)
+        .replace(new RegExp(Plantilla.plantillaTags.FECHA.MES, 'g'), persona.data.Fecha_nacimiento.Mes)
+        .replace(new RegExp(Plantilla.plantillaTags.FECHA.ANIO, 'g'), persona.data.Fecha_nacimiento.Año)
+        .replace(new RegExp(Plantilla.plantillaTags.PAIS, 'g'), persona.data.País)
+        .replace(new RegExp(Plantilla.plantillaTags.CUMBRES, 'g'), persona.data.Grandes_Cumbre)
+        .replace(new RegExp(Plantilla.plantillaTags["PICOS 8KM"], 'g'), persona.data.cantidad_picos_8km)
+}
+
+/**
+ * Actualiza el cuerpo de la tabla con los datos de la persona que se le pasa
+ * @param {Persona} Persona Objeto con los datos de la persona que queremos escribir en el TR
+ * @returns La plantilla del cuerpo de la tabla con los datos actualizados 
+ */
+Plantilla.plantillaTablaPersonas.actualiza = function (persona) {
+    return Plantilla.sustituyeTags(this.cuerpo, persona)
+}
+
+/**
+ * Función que recuperar todas las personas llamando al MS Personas
+ * @param {función} callBackFn Función a la que se llamará una vez recibidos los datos.
+ */
+
+Plantilla.recupera = async function (callBackFn){
+    let response = null
+
+    //Intento conectar con el microservicio personas
+    try{
+        const url = Frontend.API_GATEWAY + "/plantilla/getTodas"
+        response = await fetch(url)
+    }catch(error){
+        alert("Error: No se han podido acceder al API Gateway")
+        console.error(error)
+        //throw error
+    }
+
+    //Muestro todas las personas que se han descargado
+    let vectorPersonas = null
+    if (response) {
+        vectorPersonas = await response.json()
+        callBackFn(vectorPersonas.data)
+    }
+}
+
+/**
+ * Función para mostrar en pantalla todas las personas que se han recuperado de la BBDD.
+ * @param {Vector_de_personas} vector Vector con los datos de las personas a mostrar
+ */
+
+Plantilla.imprimeMuchasPersonas = function (vector) {
+    // console.log(vector) // Para comprobar lo que hay en vector
+
+    // Compongo el contenido que se va a mostrar dentro de la tabla
+    let msj = Plantilla.plantillaTablaPersonas.cabecera
+    vector.forEach(e => msj += Plantilla.plantillaTablaPersonas.actualiza(e))
+    msj += Plantilla.plantillaTablaPersonas.pie
+
+    //Borro toda la info de Article y la sustituyo por la que me interesa
+    Frontend.Article.actualizar("Listado de persona", msj)
+}
 
 /**
  * Función principal para responder al evento de elegir la opción "Home"
@@ -107,5 +214,9 @@ Plantilla.procesarAcercaDe = function () {
     this.descargarRuta("/plantilla/acercade", this.mostrarAcercaDe);
 }
 
-
-
+/**
+ * Función principal para recuperar las personas desde el MS y, posteriormente, imprimirlas.
+ */
+Plantilla.listar = function (){
+    Plantilla.recupera(Plantilla.imprimeMuchasPersonas);
+}
--- ms-plantilla/callbacks.js
+++ ms-plantilla/callbacks.js
@@ -14,10 +14,10 @@ const faunadb = require('faunadb'),
     q = faunadb.query;
 
 const client = new faunadb.Client({
-    secret: '¿¿¿ CLAVE SECRETA EN FAUNA PARA ESTA BBDD???',
+    secret: 'fnAE_hE3gqAAzadDVIMuzhcCM6PeEEtCy4SSiZ5l',
 });
 
-const COLLECTION = "¿¿¿ COLECCION ???"
+const COLLECTION = "ALPINISMO"
 
 // CALLBACKS DEL MODELO
 
@@ -58,6 +58,28 @@ const CB_MODEL_SELECTS = {
             res.status(200).json(personas)
         } catch (error) {
             res.status(500).json({ error: error.description })
+        }
+    },
+
+    /**
+     * Método para obtener todas las personas de la BBDD.
+     * @param {*} req Objeto con los parámetros que se han pasado en la llamada a esta URL 
+     * @param {*} res Objeto Response con las respuesta que se va a dar a la petición recibida
+     */
+    getTodas: async (req, res) => {
+        try {
+            let personas = await client.query(
+                q.Map(
+                    q.Paginate(q.Documents(q.Collection(COLLECTION))),
+                    q.Lambda("X", q.Get(q.Var("X")))
+                )
+            )
+            // console.log( personas ) // Para comprobar qué se ha devuelto en personas
+            CORS(res)
+                .status(200)
+                .json(personas)
+        } catch (error) {
+            CORS(res).status(500).json({ error: error.description })
         }
     },
 
